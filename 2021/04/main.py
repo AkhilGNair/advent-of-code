007461675ba6833ad3cf38d10c71a6ffa717f1d7
Akhil Nair <yup.im.aki@gmail.com>
2021-12-04 10:45:19 +0000

    04-02

--- 2021/04/main.py
+++ 2021/04/main.py
@@ -1,9 +1,7 @@
-from typing import Dict
+import re
 from collections import namedtuple
 
 import aoc
-import re
-import sys
 
 Point = namedtuple("Point", ["x", "y"])
 
@@ -26,6 +24,7 @@ class Card:
     def __init__(self, data):
         ns = re.findall(r"[0-9]+", data)
         self.data = {v: Point(get_x(k, 5), get_y(k, 5)) for k, v in enumerate(ns)}
+        self.complete = False
 
     def strike(self, n):
         try:
@@ -35,8 +34,7 @@ class Card:
             pass
 
         if self.h_line_found() or self.v_line_found():
-            print(self.score(n))
-            sys.exit(0)
+            self.complete = True
 
     def h_line_found(self):
         return not (self.EXPECTED == {p.y for p in self.data.values()})
@@ -45,12 +43,23 @@ class Card:
         return not (self.EXPECTED == {p.x for p in self.data.values()})
 
     def score(self, n):
-        print("BINGO!")
         return int(n) * sum(int(i) for i in self.data.keys())
 
+
+print("===")
 
 cards = [Card(data=data) for data in cards]
 for n in numbers:
     [card.strike(n) for card in cards]
 
-print("===")
+    for card in cards:
+        if card.complete:
+            print("BINGO!", card.score(n))
+
+            # Uncomment for part 1
+            # import sys
+            # sys.exit(0)
+
+    cards = [card for card in cards if not card.complete]
+    if not cards:
+        break

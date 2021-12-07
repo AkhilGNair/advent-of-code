8dfb7722b14cce0c118ee791aa6486e497a19aef
Akhil Nair <yup.im.aki@gmail.com>
2021-12-07 20:35:35 +0000

    07-02 :(

--- 2021/07/main.py
+++ 2021/07/main.py
@@ -1,8 +1,23 @@
-from statistics import median
+from statistics import median, mean
 
 import aoc
 
 
-data = aoc.read(sep=",", cast_as=int)
+data = aoc.read("input.txt", sep=",", cast_as=int)
 m = median(data)
 fuel = sum(abs(x - m) for x in data)
+
+print(f"Part 1 - total fuel:", int(fuel))
+
+
+def triangle_fuel(x, m):
+    t = -1 if x < m else 1
+    return 0.5 * ((x - m) * (x - m + t))
+
+min = None
+for i in range(len(data)):
+    fuel = sum(triangle_fuel(x, i) for x in data)
+    if min is None or min > fuel:
+        min = fuel
+
+print(f"Part 2 - total fuel:", int(min))

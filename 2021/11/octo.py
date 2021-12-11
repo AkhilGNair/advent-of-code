09c96c8d29fe7fc9f76c1accd8a5ff8342438596
Akhil Nair <yup.im.aki@gmail.com>
2021-12-11 12:13:19 +0000

    11-01 and 11-02

---
+++ 2021/11/octo.py
@@ -0,0 +1,84 @@
+from itertools import chain, product
+from typing import List
+
+from aoc import Point, read
+
+data = read("input.txt", sep="\n\n", cast_as=str).pop()
+N = data.index("\n")
+data = data.replace("\n", "")
+
+ADJ = {-1, 0, 1}
+ADJ = [v for v in product(ADJ, ADJ) if v != (0, 0)]
+ENOUGH_ENERGY = 9
+
+
+def print_octos(octopodes):
+    Y = 1
+    for p, e in octopodes.items():
+        if p.y == Y:
+            print("\n", end="")
+            Y += 1
+        print(e if e <= ENOUGH_ENERGY else "x", end="")
+    print("\n")
+
+
+def neighbours(p: Point) -> List[Point]:
+    return [Point(p.x + dx, p.y + dy) for dx, dy in ADJ]
+
+
+def count_flashes(octopodes) -> int:
+    return len([o for o, e in octopodes.items() if not e])
+
+
+def flash(octopodes):
+    # Find all octos about to flash
+    while (flashing_octos := [p for p, e in octopodes.items() if e > ENOUGH_ENERGY]) :
+
+        # Set popped octo energy to 0
+        for p, e in octopodes.items():
+            if e > ENOUGH_ENERGY:
+                octopodes[p] = 0
+
+        # All adjacent octos who gain some energy
+        all_neighbours = [neighbours(p) for p in flashing_octos]
+
+        # Increase energy of neighbours all at once
+        for q in chain(*all_neighbours):
+            try:
+                energy = octopodes[q]
+                # An octo who just popped can't pop again
+                if energy:
+                    octopodes[q] += 1
+            except KeyError:
+                # Octo might be on the edge of the map
+                pass
+
+    return octopodes, count_flashes(octopodes)
+
+
+def step(octopodes):
+    octopodes = {p: e + 1 for p, e in octopodes.items()}
+    return flash(octopodes)
+
+
+octopodes = {Point.from_index(i, N): int(e) for i, e in enumerate(data)}
+
+total_flashes = 0
+for i in range(100):
+    octopodes, flashes = step(octopodes)
+    total_flashes += flashes
+
+print("Part 1:", total_flashes)
+
+octopodes = {Point.from_index(i, N): int(e) for i, e in enumerate(data)}
+
+n_steps = 0
+while True:
+    octopodes, flashes = step(octopodes)
+
+    if len(set(octopodes.values())) == 1:
+        break
+
+    n_steps += 1
+
+print("Part 2:", n_steps + 1)
--- aoc/aoc/__init__.py
+++ aoc/aoc/__init__.py
@@ -1,6 +1,25 @@
 from pathlib import Path
+from typing import NamedTuple
 
 
 def read(path="input.txt", cast_as=int, sep="\n"):
     l = Path(path).read_text().strip().split(sep)
     return [cast_as(i) for i in l]
+
+def x(i, n):
+    """Returns the x position of an index in a square of length n."""
+    return i - ((i // n) * n)
+
+
+def y(i, n):
+    """Returns the y position of an index in a square of length n."""
+    return i // n
+
+class Point(NamedTuple):
+    x: int
+    y: int
+
+    @classmethod
+    def from_index(self, i:int , n: int) -> "Point":
+        return Point(x(i, n), y(i, n))
+

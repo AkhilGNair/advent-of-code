e011cfed17b33d5d237ae8039a1bf54999515c5c
Akhil Nair <yup.im.aki@gmail.com>
2021-12-18 10:31:11 +0000

    17-01

---
+++ 2021/17/fire.py
@@ -0,0 +1,65 @@
+import re
+
+
+def get_box(text):
+    return [int(n) for n in re.findall("-?\d+", text)]
+
+
+def step(px, py, vx, vy):
+    px += vx
+    py += vy
+
+    if vx > 0:
+        vx -= 1
+    elif vx < 0:
+        vx += 1
+
+    vy -= 1
+
+    return (px, py, vx, vy)
+
+
+def check(px, py, has_hit):
+
+    if px > X_MAX or py < Y_MIN:
+        return py, has_hit, False
+
+    if X_MIN <= px <= X_MAX and Y_MIN <= py <= Y_MAX:
+        has_hit = True
+        return py, has_hit, True
+
+    return py, has_hit, True
+
+
+# TARGET = "target area: x=20..30, y=-10..-5"
+TARGET = "target area: x=56..76, y=-162..-134"
+
+X_MIN, X_MAX, Y_MIN, Y_MAX = get_box(TARGET)
+
+
+def shoot(vx, vy):
+    max_py = 0
+    has_hit = False
+    v = (0, 0, vx, vy)
+
+    while True:
+        v = step(*v)
+        new_py, has_hit, keep_going = check(v[0], v[1], has_hit)
+        if not keep_going:
+            if has_hit:
+                return max_py
+            else:
+                return 0
+        if new_py > max_py:
+            max_py = new_py
+
+
+best_trajectory = 0
+max_y = 0
+
+for vx in range(1000):
+    for vy in range(1000):
+        new_y = shoot(vx, vy)
+        if new_y > max_y:
+            max_y = new_y
+            best_trajectory = (vx, vy)
